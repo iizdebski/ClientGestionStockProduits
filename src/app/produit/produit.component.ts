ef4489848d62e55dde4ca9d1052b3047e8108193
SwAppCoder <iizdepskyy112@gmail.com>
2019-12-27 22:54:24 +0200

    Gestion Stock Produits Video24

--- src/app/produit/produit.component.ts
+++ src/app/produit/produit.component.ts
@@ -1,10 +1,10 @@
-
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators} from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
-import {ProduitService} from './produit.service';
-import {Produit} from '../shared/produit.model';
+import { ProduitService } from './produit.service';
+import { Produit } from '../shared/produit.model';
+import { DataModel } from '../shared/data.model';
 
 @Component({
   selector: 'app-produit',
@@ -12,72 +12,35 @@ import {Produit} from '../shared/produit.model';
   styleUrls: ['./produit.component.css']
 })
 export class ProduitComponent implements OnInit{
-
+    
   produits: Produit[];
 
-  produitForm: FormGroup;
+  produitForm: FormGroup;    
 
-  operation: string = 'add';
+  produit: Produit = new Produit();
 
-  selectedProduit: Produit;
+  produitsModel: DataModel[];
 
-  constructor(private produitService: ProduitService, private fb: FormBuilder, private route: ActivatedRoute){
-    this.createForm();   
+  constructor(private produitService: ProduitService, private fb: FormBuilder, private route: ActivatedRoute){  
   }
 
-  ngOnInit(){
-    this.initProduit();
+  ngOnInit(){ 
     this.produits = this.route.snapshot.data.produits;
-  }
-
-  createForm(){
     this.produitForm = this.fb.group({
       ref: ['', Validators.required],
-      quantite: '',
-      prixUnitaire: ''
+      quantite:'',
+      prixUnitaire:''
     });
-  }
 
-  loadProduits(){
-    this.produitService.getAll().subscribe(
-      data => {this.produits = data},
-      error => { console.log('An error was occured.')},
-      () => { console.log('loading produits was done.')}
-    );
-  }  
+    this.produitsModel = [
+      new DataModel('id','ID','number',true,[]),
+      new DataModel('ref','Reference','string',false,[]),
+      new DataModel('quantite','Quantite','number',false,[]),
+      new DataModel('prixUnitaire','Prix Unitaire','number',false,[]),
+    ]
+  } 
 
-  addProduit(){
-    const p = this.produitForm.value;
-    this.produitService.add(p).subscribe(
-      res => {
-        this.initProduit();
-        this.loadProduits();
-      }
-    );
-  }
-
-    updateProduit(){
-      this.produitService.update(this.selectedProduit)
-      .subscribe(
-        res => {
-          this.initProduit();
-          this.loadProduits();
-        }
-      );
-    } 
-
-    initProduit(){
-      this.selectedProduit = new Produit();
-      this.createForm();
-    }
-
-    deleteProduit(){
-      this.produitService.delete(this.selectedProduit.id)
-      .subscribe(
-        res => {
-          this.selectedProduit = new Produit();
-          this.loadProduits();
-        }
-      )
-    }
-  }
+  
+  
+  
+}
